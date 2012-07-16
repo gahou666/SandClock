f043a33294d66bfa6b426bee08f0898a9c79ebbf
gahou <kimoto.8363@gmail.com>
2012-07-16 16:03:40 +0900

    Refactored sources

--- src/jp/ac/titech/cs/sandclock/SandClockActivity.java
+++ src/jp/ac/titech/cs/sandclock/SandClockActivity.java
@@ -14,21 +14,6 @@ public class SandClockActivity extends Activity {
         super.onCreate(savedInstanceState);
 		FrameLayout fl = new FrameLayout(this);
 		setContentView(fl);
-		
-		TextView tv = new TextView(this);
-    	//setContentView(tv);
-    	//fl.addView(tv);
-        LinearLayout l = new LinearLayout(this);
-        l.setOrientation(LinearLayout.VERTICAL);
-        //setContentView(l);
-        fl.addView(l);
-        l.addView(new Screen(this));
-        
-        /*
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        
-        setContentView(new ImageResizeView(this, R.drawable.t1));
-        */
+        fl.addView(new Screen(this));
     }
 }
--- src/jp/ac/titech/cs/sandclock/Screen.java
+++ src/jp/ac/titech/cs/sandclock/Screen.java
@@ -28,29 +28,24 @@ public class Screen extends View {
 		c.drawText("testString"+String.valueOf(tt), 100, 150, p);
 		tt++;
 		p.setStrokeWidth(3);
-		float[] line = {
-				287, 459,
-				3, 23,
-				3, 23,
-				577, 23,
-				577, 23,
-				293, 459,
-				293, 459,
-				577, 895,
-				577, 895,
-				3, 895,
-				3, 895,
-				287, 459};
-		c.drawLines(line, p);
-		
+		Path path = new Path();
+		path.reset();
+		path.moveTo(287, 459);
+		path.lineTo(3, 23);
+		path.lineTo(577, 23);
+		path.lineTo(293, 459);
+		path.lineTo(577, 895);
+		path.lineTo(3, 895);
+		path.lineTo(287, 459);
+		c.drawPath(path, p);
 		
 		//draw sand
 		p.setColor(Color.YELLOW);
 		p.setStyle(Paint.Style.FILL_AND_STROKE);
-		Path path = new Path();
 		float maxt = 120;
 		float t = maxt-tt;
 		float rate = (float)Math.sqrt(t/maxt);
+		if(tt>120) rate = 0;
 
 		path.reset();
 		PointF base1 = new PointF(287,459);
@@ -67,29 +62,17 @@ public class Screen extends View {
 		path.lineTo(base2.x+574-284*(1-rate)*(float)0.6, base2.y-436*(1-rate)*(float)0.6);
 		path.lineTo(base2.x+574, base2.y);
 		c.drawPath(path,p);
-		/*
-		Bitmap img;
-		
-		Resources res = this.getContext().getResources();
-		img = BitmapFactory.decodeResource(res, R.drawable.t1);
-		
-		c.drawBitmap(img,0,0,p);
-		*/
 
 		//Timer
-		
 		try {
 			TimeUnit.SECONDS.sleep(1);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 				
 		invalidate();
 		
 		//How do i get the current orientation - stack overflow
 	}
-	
-	
 }
